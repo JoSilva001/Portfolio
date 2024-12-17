0f88227897b8391fed01404d53d772d4c241e883
José <js942650@gmail.com>
2024-12-17 09:18:32 -0300

    alterações

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-2TFCFTYI.mjs";import"./chunk-6TZLWGVY.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-M67RPBL2.mjs";import"./chunk-6TZLWGVY.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
