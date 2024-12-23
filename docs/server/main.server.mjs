849257ac130deaf3f59c0b0cf099fdf100f63d4b
José <js942650@gmail.com>
2024-12-22 22:11:49 -0300

    Responsivo

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-TVL3DVDI.mjs";import"./chunk-IYFK6MOD.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-URP6YAE5.mjs";import"./chunk-S2RYALFR.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
