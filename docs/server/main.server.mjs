7f721306e57ff3cedacfc11d7037e81397a77ac9
José <js942650@gmail.com>
2024-12-22 22:38:22 -0300

    ajustes

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-URP6YAE5.mjs";import"./chunk-S2RYALFR.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-IE2WS5YU.mjs";import"./chunk-S2RYALFR.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
