0e815f5e2582be83f460e92594cd467ece7431be
José <107124086+JoSilva001@users.noreply.github.com>
2025-01-04 18:38:00 -0300

    ajustes titulo

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-SYKDAZJP.mjs";import"./chunk-S2RYALFR.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-VDCGIGML.mjs";import"./chunk-S2RYALFR.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
