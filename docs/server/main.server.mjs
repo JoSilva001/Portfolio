5cf3b64d54debb1e43ce73a27b1e25099c297f22
José <107124086+JoSilva001@users.noreply.github.com>
2025-01-04 18:27:57 -0300

    redeploy

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-IE2WS5YU.mjs";import"./chunk-S2RYALFR.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-SYKDAZJP.mjs";import"./chunk-S2RYALFR.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
