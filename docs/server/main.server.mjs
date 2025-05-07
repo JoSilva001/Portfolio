7635abf409a310d23db9ddf071b5edf4fb0442b7
José <107124086+JoSilva001@users.noreply.github.com>
2025-05-07 19:23:41 -0300

    indexando portfolio

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-A7SBZPXY.mjs";import"./chunk-YTWYOLUT.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-2QAU5LJH.mjs";import"./chunk-YTWYOLUT.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
