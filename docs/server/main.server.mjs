a14d79c74b6c1bc00aaff9cda0e36984d4c90a9d
José <107124086+JoSilva001@users.noreply.github.com>
2025-05-07 08:25:04 -0300

    add new project

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-KPIDWER6.mjs";import"./chunk-YTWYOLUT.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-A7SBZPXY.mjs";import"./chunk-YTWYOLUT.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
