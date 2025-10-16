e11ae03b80bb8ac6d5ef4714ce77e7f90f79f5cc
José <107124086+JoSilva001@users.noreply.github.com>
2025-10-16 16:19:02 -0300

    dif build

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-TIM75W3L.mjs";import"./chunk-YTWYOLUT.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-PP3IWWZ7.mjs";import"./chunk-YTWYOLUT.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
