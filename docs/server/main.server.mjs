10bbf590ede18e473ac7bddb2a2f3e3d00e65ddc
José <107124086+JoSilva001@users.noreply.github.com>
2025-10-16 16:30:53 -0300

    png tencologia mudada

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-NRBZIJ7R.mjs";import"./chunk-YTWYOLUT.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-KGHPO53F.mjs";import"./chunk-YTWYOLUT.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
