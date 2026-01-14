b1ba4fd596aac19b6a2162d8b486ccb4f09c8bde
José <107124086+JoSilva001@users.noreply.github.com>
2026-01-14 08:46:54 -0300

    deploy fix

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-IQYEZZKI.mjs";import"./chunk-KECDDRGF.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-RLZHFYOK.mjs";import"./chunk-KECDDRGF.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
